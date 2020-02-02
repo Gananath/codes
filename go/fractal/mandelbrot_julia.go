68f1d17cc2d2b4c1178d7cbc315b2a0b56b4d1d9
Gananath R <24630963+Gananath@users.noreply.github.com>
2020-02-02 12:36:08 +0530

    Update mandelbrot_julia.go

--- go/fractal/mandelbrot_julia.go
+++ go/fractal/mandelbrot_julia.go
@@ -3,6 +3,8 @@
  * 
  * A fractal generating code for mandelbrot and julia set
  * 
+ * f(z) = z^2+c
+ * 
  * Julia set is similar to mandelbrot except the c is a constant complex equation and z iterates from different location in a complex plane
  * 
  * complex plane{x=real;y=imaginary}
@@ -17,40 +19,58 @@ import (
     "image/color"
     "image/png"
     "math/cmplx"
-    //"fmt"
+    "fmt"
 )
 
+var mb,w,h,mi,zoom int
+type fractal struct{
+	width int
+	height int
+	max_iter int
+}
+
+func init(){
+	fmt.Print("Enter 0 for mandelbrot 1 for julia: ")
+	fmt.Scanln(&mb)
+	fmt.Print("Enter Width: ")
+	fmt.Scanln(&w)
+	fmt.Print("Enter Height: ")
+	fmt.Scanln(&h)
+	fmt.Print("Enter Iteration: ")
+	fmt.Scanln(&mi)
+	fmt.Print("Enter zoom: ")
+	fmt.Scanln(&zoom)
+}
 
 func main(){
-
-    width := 300
-    height := 300
-    max_iter := 30
-    
-    var img = image.NewRGBA(image.Rect(0, 0, width, height))
-
-    for x := 0; x < width; x++ {
-        for y := 0; y < height; y++ {
-            
-            // Mandelbrot Set
-            c_re := normalize(float64(x),0,float64(width),-2,2)
-            c_im := normalize(float64(y),0,float64(height),-2,2)
-            z := complex(0,0)
-            
+	frac := fractal{w,h,mi}
+    var img = image.NewRGBA(image.Rect(0, 0, frac.width, frac.height))
+	var c_re,c_im float64
+	var z complex128
+    for x := 0; x < frac.width; x++ {
+        for y := 0; y < frac.height; y++ {
+            if mb == 0{
+				c_re = float64(1/zoom)*normalize(float64(x),0,float64(frac.width),-2,2)
+				c_im = float64(1/zoom)*normalize(float64(y),0,float64(frac.height),-2,2)
+				z = complex(0,0)
+			}else if mb==1{
             // Julia Set
             // Uncomment below lines to u
-			      //c_re := -0.8 
-			      //c_im :=  0.156
-            //z_re := normalize(float64(x),0,float64(width),-2,2)
-            //z_im := normalize(float64(y),0,float64(height),-2,2)
-            //z := complex(z_re,z_im)
-            
+				c_re = -0.8 
+				c_im =  0.156
+				z_re := float64(1/zoom)*normalize(float64(x),0,float64(frac.width),-2,2)
+				z_im := float64(1/zoom)*normalize(float64(y),0,float64(frac.height),-2,2)
+				z = complex(z_re,z_im)
+			}else{
+				fmt.Println("Something went wrong...exiting")
+			}
             c := complex(c_re,c_im)
-            n:= mandelbrot_julia(c,z,max_iter)
-            if n == max_iter{
-                img.Set(x, y, color.Black)                
+            n:= mandelbrot_julia(c,z,frac.max_iter)
+            if n == frac.max_iter{
+                img.Set(x, y, color.Black)
+
             }else{
-				col :=uint8(255 - int(n * 255 / max_iter))
+				col :=uint8(255 - int(n * 255 / frac.max_iter))
 				colr := color.RGBA{col, col, col, 255}
                 img.Set(x, y, colr)
             }
